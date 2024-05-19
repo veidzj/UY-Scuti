24d8f4b3739face9c91b0d1ba280c8ee505470f3
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-19 10:51:44 -0300

    refactor: move mock to own folder

--- tests/presentation/controllers/account/add-account-controller.spec.ts
+++ tests/presentation/controllers/account/add-account-controller.spec.ts
@@ -1,5 +1,5 @@
-import { type AddAccount } from '@/domain/usecases/account/add-account'
 import { AddAccountController } from '@/presentation/controllers/account/add-account-controller'
+import { AddAccountSpy } from '@/tests/domain/mocks/add-account-mock'
 
 const mockInput = (): AddAccountController.Input => ({
   username: 'any_username'
@@ -7,13 +7,6 @@ const mockInput = (): AddAccountController.Input => ({
 
 describe('AddAccountController', () => {
   test('Should call AddAccount with correct values', async() => {
-    class AddAccountSpy implements AddAccount {
-      public input: AddAccount.Input
-
-      public async add(input: AddAccount.Input): Promise<void> {
-        this.input = input
-      }
-    }
     const addAccountSpy = new AddAccountSpy()
     const sut = new AddAccountController(addAccountSpy)
     const input = mockInput()
