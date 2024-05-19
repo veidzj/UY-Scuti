300b794d61f98619f4f8db7cafe1bf31f5302ca9
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-19 10:52:49 -0300

    refactor: move sut creation to a factory

--- tests/presentation/controllers/account/add-account-controller.spec.ts
+++ tests/presentation/controllers/account/add-account-controller.spec.ts
@@ -1,14 +1,27 @@
 import { AddAccountController } from '@/presentation/controllers/account/add-account-controller'
 import { AddAccountSpy } from '@/tests/domain/mocks/add-account-mock'
 
+interface Sut {
+  sut: AddAccountController
+  addAccountSpy: AddAccountSpy
+}
+
+const makeSut = (): Sut => {
+  const addAccountSpy = new AddAccountSpy()
+  const sut = new AddAccountController(addAccountSpy)
+  return {
+    sut,
+    addAccountSpy
+  }
+}
+
 const mockInput = (): AddAccountController.Input => ({
   username: 'any_username'
 })
 
 describe('AddAccountController', () => {
   test('Should call AddAccount with correct values', async() => {
-    const addAccountSpy = new AddAccountSpy()
-    const sut = new AddAccountController(addAccountSpy)
+    const { sut, addAccountSpy } = makeSut()
     const input = mockInput()
     await sut.handle(input)
     expect(addAccountSpy.input).toEqual(input)
