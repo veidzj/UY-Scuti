0a79581ff1cf90e2ae41fedccc97cbb7cdf32352
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-21 20:07:29 -0300

    feat: ensure AddAccountController calls Validation with correct values

--- src/presentation/controllers/account/add-account-controller.ts
+++ src/presentation/controllers/account/add-account-controller.ts
@@ -1,14 +1,19 @@
 import { type Controller } from '@/presentation/protocols/controller'
 import { type HttpResponse } from '@/presentation/protocols/http-response'
+import { type Validation } from '@/presentation/protocols/validation'
 import { type AddAccount } from '@/domain/usecases/account/add-account'
 import { conflict, created, serverError } from '@/presentation/helpers/http-helper'
 import { EmailInUseError } from '@/domain/errors/account/email-in-use-error'
 
 export class AddAccountController implements Controller {
-  constructor(private readonly addAccount: AddAccount) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly addAccount: AddAccount
+  ) {}
 
   public async handle(request: AddAccountController.Request): Promise<HttpResponse> {
     try {
+      this.validation.validate(request)
       const accountId = await this.addAccount.add(request)
       return created({ accountId })
     } catch (error) {
