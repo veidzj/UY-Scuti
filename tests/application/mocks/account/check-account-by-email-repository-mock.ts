5c4a3e33e7edbd2c5ff12cd0c4dc7e6e75174497
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-22 12:28:14 -0300

    fix: missing protocol import

--- tests/application/mocks/account/check-account-by-email-repository-mock.ts
+++ tests/application/mocks/account/check-account-by-email-repository-mock.ts
@@ -1,4 +1,6 @@
-export class CheckAccountByEmailRepositorySpy implements CheckAccountByEmailRepositorySpy {
+import { type CheckAccountByEmailRepository } from '@/application/protocols/account/check-account-by-email-repository'
+
+export class CheckAccountByEmailRepositorySpy implements CheckAccountByEmailRepository {
   public email: string
   public output: boolean = false
 
