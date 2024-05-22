2bebddcaf6a8ecab116519769dcfa6f9c6a07cb1
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-22 12:25:07 -0300

    feat: ensure DbAddAcount throws EmailInUseError if CheckAccountByEmailRepository returns true

--- src/application/usecases/account/db-add-account.ts
+++ src/application/usecases/account/db-add-account.ts
@@ -1,11 +1,15 @@
 import { type CheckAccountByEmailRepository } from '@/application/protocols/account/check-account-by-email-repository'
+import { EmailInUseError } from '@/domain/errors/account/email-in-use-error'
 import { type AddAccount } from '@/domain/usecases/account/add-account'
 
 export class DbAddAccount implements AddAccount {
   constructor(private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository) {}
 
   public async add(input: AddAccount.Input): Promise<string> {
-    await this.checkAccountByEmailRepository.check(input.email)
+    const emailInUse = await this.checkAccountByEmailRepository.check(input.email)
+    if (emailInUse) {
+      throw new EmailInUseError()
+    }
     return ''
   }
 }
