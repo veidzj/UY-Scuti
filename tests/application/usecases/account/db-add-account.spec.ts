7a474f78ca38915a3b4ee55567e0d740d474564a
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-22 12:23:40 -0300

    test: ensure DbAddAccount throws if CheckAccountByEmailRepository throws

--- tests/application/usecases/account/db-add-account.spec.ts
+++ tests/application/usecases/account/db-add-account.spec.ts
@@ -23,4 +23,11 @@ describe('DbAddAccount', () => {
     await sut.add(addAccountInput)
     expect(checkAccountByEmailRepositorySpy.email).toBe(addAccountInput.email)
   })
+
+  test('Should throw if CheckAccountByEmailRepository throws', async() => {
+    const { sut, checkAccountByEmailRepositorySpy } = makeSut()
+    jest.spyOn(checkAccountByEmailRepositorySpy, 'check').mockRejectedValueOnce(new Error())
+    const promise = sut.add(mockAddAccountInput())
+    await expect(promise).rejects.toThrow()
+  })
 })
