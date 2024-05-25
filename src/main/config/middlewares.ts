aa902fbfed7bf4ec407844d9cc57b4e2e08471df
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-25 20:59:26 -0300

    feat: add middlewares config

---
+++ src/main/config/middlewares.ts
@@ -0,0 +1,11 @@
+import { type Express } from 'express'
+
+import { bodyParser } from '@/main/middlewares/body-parser'
+import { contentType } from '@/main/middlewares/content-type'
+import { cors } from '@/main/middlewares/cors'
+
+export const middlewares = (app: Express): void => {
+  app.use(bodyParser)
+  app.use(contentType)
+  app.use(cors)
+}
